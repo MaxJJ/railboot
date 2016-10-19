73eed517d7f392f23587f439d589dd3505578d06
MaxJJ <mjefremov@gmail.com>
2016-10-19 22:03:50 +0300

    jaspertest two

--- src/main/java/lv/rtme/Jaspertest.java
+++ src/main/java/lv/rtme/Jaspertest.java
@@ -5,18 +5,17 @@
  */
 package lv.rtme;
 
-import java.io.FileOutputStream;
 import java.util.HashMap;
 import java.util.Map;
-import static javassist.bytecode.InstructionPrinter.print;
-import lv.rtme.entities.Points;
+import lv.rtme.entities.TestBeany;
 import net.sf.jasperreports.engine.JRException;
-import net.sf.jasperreports.engine.JRExporterParameter;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperPrintManager;
 import net.sf.jasperreports.engine.data.JRAbstractBeanDataSource;
 import net.sf.jasperreports.engine.data.JRBeanArrayDataSource;
 import net.sf.jasperreports.engine.export.JRPdfExporter;
+import net.sf.jasperreports.view.JasperViewer;
 
 /**
  *
@@ -26,27 +25,28 @@ public class Jaspertest {
     
     public static void main(String[] args) throws JRException {
         
-        Points[] myBeans = new Points[1];
-        myBeans[0]=new Points("станция","Рига-Краста","098654");
+        TestBeany[] myBeans = new TestBeany[1];
         
-        JRAbstractBeanDataSource beanDataSource = new JRBeanArrayDataSource(myBeans, true);
         
+       myBeans[0]=new TestBeany("Мой охеренный текст");
+        
+        JRAbstractBeanDataSource beanDataSource = new JRBeanArrayDataSource(myBeans, false);
+       
+       
         Map<String,Object> params = new HashMap<>();
         
-      JasperPrint jasperPrint = JasperFillManager.fillReport("C:\\Users\\User\\Documents\\NetBeansProjects\\railboot\\src\\main\\resources\\Blank_A4.jasper", params, beanDataSource);
+      JasperPrint jasperPrint = JasperFillManager.fillReport("C:\\Users\\Maksims\\Documents\\NetBeansProjects\\lv\\rtme\\railboot\\src\\main\\resources\\testingJR.jasper", params, beanDataSource);
       
       
       JRPdfExporter exporter = new JRPdfExporter();
-//          exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
-//          exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(outputStream));
-//          SimplePdfExporterConfiguration configuration = new SimplePdfExporterConfiguration();
-//          
-//          configuration.setPermissions(PdfWriter.AllowCopy | PdfWriter.AllowPrinting);
-//          exporter.setConfiguration(configuration);
-exporter.setParameter(JRExporterParameter.JASPER_PRINT, print);
-			exporter.setParameter(JRExporterParameter.OUTPUT_STREAM, new FileOutputStream(reportName + ".pdf")); // your output goes here
-          exporter.exportReport();
+//        JRViewer view = new JRViewer(jasperPrint);
+//        
+//        view.setVisible(true);
+        
+        JasperViewer viewer = new JasperViewer(jasperPrint);
+    viewer.setVisible(true);
     
+        JasperPrintManager.printReport(jasperPrint, true);
     }
  
 
